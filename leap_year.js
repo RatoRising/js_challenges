578db4548113b6dd9bca8f204bb35185e8de62e8
RatoRising <reinaneves@hotmail.com>
2023-01-11 12:17:32 -0300

    refactor leap_year code

--- leap_year.js
+++ leap_year.js
@@ -1,11 +1,6 @@
-let year = 2020;
-
-function leapYear() {
-	if ((year % 400 === 0 && year % 100 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
-		return 'Leap year';
-	}
-
-	return 'not leap year';
+function leapYear(year) {
+   let isLeapYear = (year % 400 === 0 && year % 100 === 0) || (year % 4 === 0 && year % 100 !== 0)
+   return isLeapYear ? 'Leap year' : 'not leap year';
 }
 
-console.log(leapYear());
+console.log(leapYear(1992));
